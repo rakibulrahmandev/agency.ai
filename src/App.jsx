e293c0c3f586af97ef066eef3fb2ca55be4c6426
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-21 21:10:40 +0600

    create header component and header links done with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import Header from "./components/Header/Header";
+
 export default function App() {
+
+    const [theme, setTheme] = useState('light')
+
     return (
         <>
-            
+            <div className="dark:bg-black relative">
+                <Header theme={theme} setTheme={setTheme} />
+            </div>
         </>
     )
 }
