6a5b55bc514cc9485da6057f799489591a0d37b1
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-22 16:46:10 +0600

    title component structure done

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,8 +1,9 @@
-export default function Title() {
+export default function Title({title, description}) {
     return (
         <>
-            <div>
-                
+            <div className="w-auto">
+                <h2 className="text-3xl sm:text-5xl font-medium">{title}</h2>
+                <p className="max-w-lg text-center text-gray-500 dark:text-white/75 mb-6">{description}</p>
             </div>
         </>
     )
