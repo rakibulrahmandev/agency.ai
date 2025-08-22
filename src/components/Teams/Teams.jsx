0fa5ce4849c5ca42f4877b75a5dbf70c99a29240
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-22 22:09:55 +0600

    teams section title also done

--- src/components/Teams/Teams.jsx
+++ src/components/Teams/Teams.jsx
@@ -1,8 +1,10 @@
+import Title from "../Title/Title";
+
 export default function Teams() {
     return (
         <>
-            <div>
-                
+            <div className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-800 dark:text-white">
+                <Title title="Meet the team" description="A passionate team of digital experts dedicated to your brands success." />
             </div>
         </>
     )
