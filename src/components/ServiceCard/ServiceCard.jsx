e2d0b6954959081fe10a6da6432ecd18f7be06e7
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-22 22:14:40 +0600

    fix service card height

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -2,8 +2,8 @@ export default function ServiceCard({service}) {
 
     return (
         <>
-            <div className="relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700">
-                <div className="flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-gray-900 z-10 relative">
+            <div className="overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700">
+                <div className="flex items-center gap-10 p-8 rounded-[10px] h-full bg-white dark:bg-gray-900">
                     <div className="bg-gray-100 dark:bg-gray-700 rounded-full">
                         <img src={service?.icon} alt="icons" className="max-w-24 bg-white dark:bg-gray-900 rounded-full m-2" />
                     </div>
