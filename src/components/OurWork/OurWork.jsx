6cb298f20ef38263a0a0dd4828e2078f46fef476
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-22 22:16:19 +0600

    change background color when dark mode

--- src/components/OurWork/OurWork.jsx
+++ src/components/OurWork/OurWork.jsx
@@ -23,7 +23,7 @@ export default function OurWork() {
 
     return (
         <>
-            <div id="our-work" className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white">
+            <div id="our-work" className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white dark:bg-gray-900/70">
                 <Title title="Our Work" description="From strategy to execution, we craft digital solutions that move your business forward." />
                 <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl">
                     {
