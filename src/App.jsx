587e6590e420124ff726018da08ae7ce08008e12
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-23 19:45:51 +0600

    create contact component and mount this component in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ export default function App() {
                 <Services />
                 <OurWork />
                 <Teams />
+                <Contact />
             </div>
         </>
     )
